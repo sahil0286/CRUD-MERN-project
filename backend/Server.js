c36c146da1e47d74722749c113ea0adadf859200
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-31 11:30:21 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config()
 
+const routes = require("./routes/TaskRoute");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,6 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>{console.log("MongoDB Connected")})
 .catch((error)=>{console.log(error)})
 
-
+app.use("/api",routes);
 
 app.listen(PORT,()=>{console.log(`Server is running on PORT: ${PORT}`);});
