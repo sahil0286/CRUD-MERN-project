f7062f90493b20a06c5859a7170f9012d0a90a60
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-31 11:40:58 +0530

    Update List.js

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -5,7 +5,12 @@ import axios from "axios";
 import { baseURL } from "../utils/constant";
 
 const List = ({ id, task, setUpdateUI, updateMode }) => {
-  
+  const removeTask = () => {
+    axios.delete(`${baseURL}/delete/${id}`).then((res) => {
+      console.log(res);
+      setUpdateUI((prevState) => !prevState);
+      alert("D : Task Deleted");
+    });
   };
 
   return (
