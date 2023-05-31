13818b6610a6df7275c6e1aa88e4d1d538225ce8
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-31 11:28:39 +0530

    added saveTask

--- backend/controllers/TaskControllers.js
+++ backend/controllers/TaskControllers.js
@@ -5,3 +5,17 @@ module.exports.getTask= async (req, res) => {
     res.send(tasks);
 };
 
+module.exports.saveTask=  (req, res) => {
+    const { task } = req.body;
+
+    TaskModel.create({ task })
+    .then((data)=>{
+        console.log("Task Saved successfully")
+        res.status(201).send(data);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send({ error : err, msg: "Task failed to Saved"});
+    });
+};
+
